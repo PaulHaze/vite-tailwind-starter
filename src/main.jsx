727ae224206f2435f417448a752067f0c08d8010
PaulHaze <dev@artemist.com.au>
2022-01-13 09:00:15 +1100

    add redux

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/main.scss';
-import './index.css';
+// import './index.css';
 import { Home } from './views';
 // import App from './App';
 
